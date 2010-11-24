d37beaef0b482936e359de84a9dd5ec8783b4059
Michael Eickenberg <michael@cice-online.net>
2010-11-24 14:58:33 +0100

    added one array test

---
+++ test/unit/array_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ArrayTest < ActiveSupport::TestCase
+  
+  
+  
+  test "an array of 10 entries should have 10x /array/data/value" do
+    arr=(1..10).to_a.map &:to_s
+    xml=arr.to_xml_rpc
+    
+    assert_select xml, '/array/data/value', 10
+  end
+  
+  
+  
+end
