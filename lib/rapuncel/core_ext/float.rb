f7fe41b7427b574f54c8432ad01a3195794ad212
Michael Eickenberg <michael@cice-online.net>
2010-11-24 18:56:27 +0100

    something changed in float.rb

--- lib/rapuncel/core_ext/float.rb
+++ lib/rapuncel/core_ext/float.rb
@@ -7,4 +7,11 @@ class Float
   def to_xml_rpc_name #we can just change this function. see why in object.rb
     "double"
   end
+  
+  
+  def from_xml_rpc xml_node
+    warn "Node given (name of #{xml_node.name}) is not a 'double' node, name should be 'double'. Will still parse, at your risk." unless 
+    
+    
+  end
 end
