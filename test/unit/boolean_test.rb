b5326aac164bf89aedb6a6cb0ad296b9a37f410a
Michael Eickenberg <michael@cice-online.net>
2010-11-24 19:53:43 +0100

    Boolean.from_xml_rpc now works, with tests. IMPORTANT: need to choose convention for default value. Right now only 1 is true, 0, but also EVERYTHING else is FALSE. this seems to oppose ruby conventions. switch around?

--- test/unit/boolean_test.rb
+++ test/unit/boolean_test.rb
@@ -11,4 +11,24 @@ class BooleanTest < ActiveSupport::TestCase
     assert_select false.to_xml_rpc, '/boolean', 1
     assert_select false.to_xml_rpc, '/boolean', '0'
   end
+  
+  test "true.to_xml_rpc reparsed must be true" do
+    xml=true.to_xml_rpc
+    xml_node = Nokogiri::XML(xml).children.first
+    
+    assert Boolean.from_xml_rpc(xml_node) #I assume assert takes a boolean?
+    assert Boolean.from_xml_rpc(xml_node) == true #is this different? just in case the other function gives back something non nil/false which is not necessarily true
+  end
+  
+  test "false.to_xml_rpc reparsed must be false" do
+    xml=false.to_xml_rpc
+    xml_node = Nokogiri::XML(xml).children.first
+    
+    assert !Boolean.from_xml_rpc(xml_node)
+  end
+  
+  test "anything else should evaluate to what now?" do
+    assert true
+  end
+  
 end
