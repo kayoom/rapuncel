abe9d6d2b1100744ae59b84ed172761d8ec25661
Michael Eickenberg <michael@cice-online.net>
2010-11-24 20:17:08 +0100

    Added from_xml_rpc to array including working tests.

--- lib/rapuncel/core_ext/array.rb
+++ lib/rapuncel/core_ext/array.rb
@@ -1,5 +1,5 @@
 require 'builder'
-
+require 'nokogiri'
 
 class Array
 
@@ -14,5 +14,27 @@ class Array
         end
       end
     end
+  end
+  
+  
+  def self.from_xml_rpc xml_node
+    warn "Warning: This is not an array-node (It is a(n) #{xml_node.name}.). Parsing may go wrong. Continuing at your risk" unless ['array'].include? xml_node.name.downcase
+    
+    data_node=xml_node.children.first
+    #child of xml_node should be a data tag
+    warn "Warning: Child of array node is not a 'data' node, it is a #{data_node.name}. Continuing at your risk" unless ['data'].include? data_node.name.downcase
+    
+    values=data_node.children
+    
+    values.map do |value|
+      warn "Value tag not present in child of child of array node. The tag is #{value.name}. Continuing at your risk" unless ['value'].include? value.name.downcase
+      
+      #the object is inside this value node
+      obj=value.children.first
+      #we parse it using object
+      Object.from_xml_rpc obj
+    end
+    
+    
   end
 end
--- test/unit/array_test.rb
+++ test/unit/array_test.rb
@@ -8,4 +8,37 @@ class ArrayTest < ActiveSupport::TestCase
 
     assert_select xml, '/array/data/value', 10
   end
+  
+  def test_array arr
+    xml = arr.to_xml_rpc
+    
+    xml_node = Nokogiri::XML(xml).children.first
+    
+    reparsed = Array.from_xml_rpc xml_node
+    
+    puts "arr.to_s = #{arr.to_s}"
+    puts "reparsed.to_s = #{reparsed.to_s}"
+    
+    reparsed
+  end
+  
+  
+  test "An array, converted to xml and parsed back should be itself" do
+    arr1 = (1..10).to_a
+    arr2 = arr1.map &:to_s
+    
+    arr3 = [arr1, arr2]
+    
+    arr4 = ["hello", arr3]
+    
+    reparsed1 = test_array arr1
+    reparsed2 = test_array arr2
+    reparsed3 = test_array arr3
+    reparsed4 = test_array arr4
+    
+    [reparsed1, reparsed2, reparsed3, reparsed4].zip([arr1, arr2, arr3, arr4]).each do |b|
+      assert_equal *b
+    end
+    
+  end
 end
