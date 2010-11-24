03fac10766de9a18567aa60fa0552f7e39de44a7
Marian Theisen <marian@cice-online.net>
2010-11-24 12:40:10 +0100

    another test

--- test/unit/request_test.rb
+++ test/unit/request_test.rb
@@ -2,10 +2,18 @@ require 'test_helper'
 require 'ruby-debug'
 
 class RequestTest < ActiveSupport::TestCase
+  def get_test_request
+    Rapuncel::Request.new 'test_method', "one argument", "another"
+  end
+  
   test "Serialized request should have 1 methodCall tag" do
-    request = Rapuncel::Request.new 'test_method', "one argument", "another"
-    
-    xml = request.to_xml_rpc
+    xml = get_test_request.to_xml_rpc
     assert_select xml, '/methodCall', 1
+  end
+  
+  test "Serialized request should contain methodName" do
+    xml = get_test_request.to_xml_rpc
+    
+    assert_select xml, '/methodCall/methodName', 'test_method'
   end
 end
