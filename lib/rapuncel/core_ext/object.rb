551c8cc290ef4154b358da8ddd5a57d624d901d2
Michael Eickenberg <michael@cice-online.net>
2010-11-24 12:17:34 +0100

    changed warnings in object.rb

--- lib/rapuncel/core_ext/object.rb
+++ lib/rapuncel/core_ext/object.rb
@@ -3,14 +3,14 @@ require 'builder'
 class Object
   
   def to_xml_rpc b=Rapuncel.get_builder
-    warn "No specific to_xml_rpc method written for this object. The result may not be what you expect."
-    
+    warn "Using Object.to_s"
     b.tag! to_xml_rpc_name.to_sym, to_s
     
   end
   
   protected
   def to_xml_rpc_name
+    warn "No specific to_xml_rpc_name method written for this object. The result may not be what you expect."
     self.class.to_s.downcase
   end
   
