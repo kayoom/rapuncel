f2392a0303f8aecd4451ad878dff3113eeceda91
Michael Eickenberg <michael@cice-online.net>
2010-11-24 14:21:51 +0100

    array without block variables

--- lib/rapuncel/core_ext/array.rb
+++ lib/rapuncel/core_ext/array.rb
@@ -3,12 +3,12 @@ require 'builder'
 
 class Array
   
-  def to_xml_rpc b
+  def to_xml_rpc b=Rapuncel.get_builder
     
-    b.array do |b|
-      b.data do |b|
+    b.array do 
+      b.data do 
         self.each do |array_entry|
-          b.value do |b|
+          b.value do 
             array_entry.to_xml_rpc b
           end
         end
