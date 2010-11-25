dd267783f458e48cd4dfc271d88bc4326c53929c
Marian Theisen <marian@cice-online.net>
2010-11-25 10:45:00 +0100

    fixed hash

--- lib/rapuncel/core_ext/hash.rb
+++ lib/rapuncel/core_ext/hash.rb
@@ -56,12 +56,13 @@ class Hash
   def self.from_xml_rpc xml_node
     warn "The given xml_node is a #{xml_node.name}, not a 'struct'. Continuing at your risk" unless ['struct'].include? xml_node.name
     
-    keys_and_values = xml_node.xpath('/struct/member')
+    keys_and_values = xml_node.xpath('./member')
     
     hash = new
     keys_and_values.each do |kv|
-      key = kv.xpath('.//name').first.text.to_sym
-      value = Object.from_xml_rpc kv.xpath('.//value').first.children.first
+      key = kv.xpath('./name').first.text.to_sym
+      # debugger
+      value = Object.from_xml_rpc kv.xpath('./value').first.children.first
       hash[key]=value
       
     end
