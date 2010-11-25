1108aa922ce65636002bd1d489929795394ce792
Michael Eickenberg <michael@cice-online.net>
2010-11-25 11:27:18 +0100

    updated boolean test

--- test/unit/boolean_test.rb
+++ test/unit/boolean_test.rb
@@ -16,15 +16,15 @@ class BooleanTest < ActiveSupport::TestCase
     xml=true.to_xml_rpc
     xml_node = Nokogiri::XML(xml).children.first
     
-    assert Boolean.from_xml_rpc(xml_node) #I assume assert takes a boolean?
-    assert Boolean.from_xml_rpc(xml_node) == true #is this different? just in case the other function gives back something non nil/false which is not necessarily true
+    assert Rapuncel::Boolean.from_xml_rpc(xml_node) #I assume assert takes a boolean?
+    assert Rapuncel::Boolean.from_xml_rpc(xml_node) == true #is this different? just in case the other function gives back something non nil/false which is not necessarily true
   end
   
   test "false.to_xml_rpc reparsed must be false" do
     xml=false.to_xml_rpc
     xml_node = Nokogiri::XML(xml).children.first
     
-    assert !Boolean.from_xml_rpc(xml_node)
+    assert !Rapuncel::Boolean.from_xml_rpc(xml_node)
   end
   
   test "anything else should evaluate to what now?" do
